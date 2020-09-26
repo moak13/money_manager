4591ab7efd2325165e34c0a32f1b18e35aea064f
Michael Kalango <michaelkalango@gmail.com>
2020-09-26 23:41:18 +0100

    clean up

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,5 +1,4 @@
 import 'package:get_it/get_it.dart';
-import 'package:money_manager/services/transaction_service.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 GetIt locator = GetIt.instance;
@@ -7,5 +6,4 @@ GetIt locator = GetIt.instance;
 Future<void> setupLocator() {
   locator.registerLazySingleton(() => NavigationService());
   locator.registerLazySingleton(() => DialogService());
-  locator.registerLazySingleton(() => TransactionService());
 }
--- lib/services/transaction_service.dart
+++
@@ -1,6 +0,0 @@
-import 'package:money_manager/models/transaction_model.dart';
-
-class TransactionService {
-  final _transactionData = List<TransactionModel>();
-  List<TransactionModel> get transactionData => _transactionData;
-}
