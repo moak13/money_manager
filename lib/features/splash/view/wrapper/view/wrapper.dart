9849153d5d6e63786bd021df7035dfa79d897a19
Michael Kalango <michaelkalango@gmail.com>
2020-09-26 03:25:39 +0100

    wrapper view and viewmodel

---
+++ lib/features/splash/view/wrapper/view/wrapper.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../../../../../view_models/wrapper_viewmodel.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ViewModelBuilder<WrapperViewModel>.reactive(
+      builder: (context, model, child) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text('Money Manager'),
+            centerTitle: true,
+            elevation: 0,
+          ),
+          body: Center(
+            child: Text('Wrapper View'),
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+              backgroundColor: Colors.white,
+              currentIndex: model.currentIndex,
+              onTap: model.setIndex,
+              items: [
+                BottomNavigationBarItem(
+                  icon: Icon(FontAwesomeIcons.home),
+                  title: Text('Home'),
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(FontAwesomeIcons.chartPie),
+                  title: Text('Chart'),
+                ),
+              ]),
+        );
+      },
+      viewModelBuilder: () => WrapperViewModel(),
+    );
+  }
+}
